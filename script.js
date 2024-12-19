82a07aa5e85c427e0a50231e9d8b9aa78a4da8a1
Roy Mamon <mamon8521@gmail.com>
2024-12-19 18:48:50 +0200

    wrote the functions and an "operate" function.

---
+++ script.js
@@ -0,0 +1,25 @@
+//functions
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => {
+    if (b === 0) {
+        return "Error: Division by zero";
+    }
+    return a / b;
+};
+
+const operate = (operation, a, b) => {
+    switch (operation) {
+        case "add":
+            return add(a, b);
+        case "subtract":
+            return subtract(a, b);
+        case "multiply":
+            return multiply(a, b);
+        case "divide":
+            return divide(a, b);
+        default:
+            return "Invalid operation";
+    }
+};
