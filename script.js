591ee28aa6ff5e671b37574dabe979a82c56573d
Roy Mamon <mamon8521@gmail.com>
2024-12-19 19:15:08 +0200

    improved dark-mode button: used localStorage to remember whether the user prefers dark-mode or not.

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 //dark mode
 const darkModeToggle = document.getElementById("dark-mode-toggle");
+
+if (localStorage.getItem("dark-mode") === "enabled") {
+    document.body.classList.add("dark-mode");
+}
+
 darkModeToggle.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("dark-mode", "enabled");
+    } else {
+        localStorage.setItem("dark-mode", "disabled");
+    }
 });
 //functions
 const add = (a, b) => a + b;
